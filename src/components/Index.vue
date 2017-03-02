9526eefd61aab4d1e9460b7b9bcdb1a77f1630c4
枫林渡 <81548634@qq.com>
2017-03-02 19:07:24 +0800

    上传目录;9C

---
+++ src/components/Index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <el-input placeholder="请输入内容" v-model="name">
+          <el-button slot="append" icon="search"></el-button>
+        </el-input>
+        <el-button type="primary" >不查询直接跳过</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+        name:''
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'
+Vue.use(Router)
+Vue.use(ElementUI)
+export default new Router({
+  routes: [
+    {
+      path: '/index',
+      name: 'index',
+      component: Index
+    }
+  ]
+})
